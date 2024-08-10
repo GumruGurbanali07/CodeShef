3d857c8d608d3feb57ce811291260fd96e33d8f0
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-08-10 23:56:04 +0400

    commit

---
+++ Codeshef/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        
+            if (x == 25)
+            {
+                Console.WriteLine("CHRISTMAS");
+            }
+            else
+            {
+                Console.WriteLine("ORDINARY");
+            }      
+
+
+    }
+}
