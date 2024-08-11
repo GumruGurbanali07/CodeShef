da0e47ed36d7da12027ca4b377284c89cea47599
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-08-11 16:33:59 +0400

    commits

--- Codeshef/Program.cs
+++ Codeshef/Program.cs
@@ -142,3 +142,199 @@ public class Test
 
 -------------------------------------------------------------------------------------------
 
+Double Rent
+public class Test
+{
+    public static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine(2 * x);
+
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Thala For A Reason
+public class Test
+{
+    public static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        if (x == 7)
+        {
+            Console.WriteLine("THALA");
+        }
+        else
+        {
+            Console.WriteLine("SADGE");
+
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Lucky Clover
+public class Test
+{
+    public static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int z = 4 + ((x - 1) * 3);
+        Console.WriteLine(z);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Good Turn
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            string[] st = Console.ReadLine().Split(' ');
+            int x = int.Parse(st[0]);
+            int y = int.Parse(st[1]);
+            if (x + y > 6)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Devouring Donuts
+public class Test
+{
+    public static void Main()
+    {
+        string[] st = Console.ReadLine().Split(' ');
+        int x = int.Parse(st[0]);
+        int y = int.Parse(st[1]);
+        Console.WriteLine(x * y);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Add Two Numbers
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            string[] st = Console.ReadLine().Split(' ');
+            int x = int.Parse(st[0]);
+            int y = int.Parse(st[1]);
+            Console.WriteLine(x + y);
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Summer Time
+public class Test
+{
+    public static void Main()
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        if (n > 35)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Advitiya
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n == 16 || n == 17 || n == 18)
+        {
+            Console.WriteLine("ADVITIYA");
+        }
+        else
+        {
+            Console.WriteLine("WAITING FOR ADVITIYA");
+        }
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+AC Please
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string result = (x > 30) ? "YES" : "NO";
+        Console.WriteLine(result);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Election Hopes
+public class Test
+{
+    public static void Main()
+    {
+        string[] st = Console.ReadLine().Split(' ');
+        int x = int.Parse(st[0]);
+        int y = int.Parse(st[1]);
+        string result = (x >= 2 * y) ? "Yes" : "No";
+        Console.WriteLine(result);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+Minimum Wage
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string result = (n > 11) ? "YES" : "NO";
+        Console.WriteLine(result);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
+
+public class Test
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string result = (n < 8) ? "LESS" : (n == 8) ? "PERFECT" : "MORE";
+        Console.WriteLine(result);
+    }
+}
+
+---------------------------------------------------------------------------------------------
+
